0e15c15434e62ac2067d80bce30aea16f023686e
anchery <35939871+anchery@users.noreply.github.com>
2020-07-18 22:31:01 -0500

    DDL component

---
+++ BlazorComponent/Data/DDComponentModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorComponent.Data
+{
+    public class DDComponentModel
+    {
+        public string Caption { get; set; }
+        public List<string> ddlItems { get; set; }
+    }
+}
